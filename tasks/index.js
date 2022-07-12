04d0869215cd0195279a7fad07bea4188694367d
truongvihuy <truongvihuy@bacoor.co>
2022-07-12 08:46:16 +0700

    chore: index file

---
+++ tasks/index.js
@@ -0,0 +1,19 @@
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+require('./utils/accounts') // ok
+require('./utils/apply-tomoz') // ok
+require('./utils/balance') // ok
+require('./utils/block') // ok
+require('./utils/create-accounts') // ok
+require('./utils/generate-abi') // ok
+require('./utils/mint-nft') // ok
+require('./utils/mint') // ok
+require('./utils/transfer') // use multi transfer (multi transfer)
+
+// test contract
+// require('./sign')
+// require('./sign-nftitem')
+// require('./sign-nftpart')
+// require('./sign-scholarship')
+// require('./test')
